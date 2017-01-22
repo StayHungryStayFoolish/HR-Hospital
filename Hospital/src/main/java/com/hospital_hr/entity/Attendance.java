776e22585b45b017c867fbcdae806c133d48d4d0
StayHungryStayFoolish <bonismo@hotmail.com>
2017-01-22 23:38:17 +0800

    commit

---
+++ Hospital/src/main/java/com/hospital_hr/entity/Attendance.java
@@ -0,0 +1,11 @@
+package com.hospital_hr.entity;
+
+import com.baomidou.mybatisplus.annotations.TableName;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午11:32 on 17/1/22.
+ */
+@TableName
+public class Attendance extends Model{
+}
