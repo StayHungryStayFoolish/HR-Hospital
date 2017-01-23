0a1f17d29a5c0c76cec453b6d0ca7abff7dfeb65
StayHungryStayFoolish <bonismo@hotmail.com>
2017-01-23 23:17:23 +0800

    commit

--- Hospital/src/main/java/com/hospital_hr/entity/Attendance.java
+++ Hospital/src/main/java/com/hospital_hr/entity/Attendance.java
@@ -1,6 +1,7 @@
 package com.hospital_hr.entity;
 
 import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableName;
 
@@ -10,11 +11,11 @@ import java.util.Date;
 /**
  * Created by bonismo@hotmail.com
  * 下午11:32 on 17/1/22.
- *
+ * <p>
  * 考勤表
  */
 @TableName("attendance")
-public class Attendance extends Model<Attendance> implements Serializable{
+public class Attendance extends Model<Attendance> implements Serializable {
 
     @TableId
     private Integer id;
@@ -37,8 +38,116 @@ public class Attendance extends Model<Attendance> implements Serializable{
     // 日志
     private String notes;
 
+    @TableField(exist = false)
+    private Employee employee;
+
     @Override
     protected Serializable pkVal() {
-        return null;
+        return this.id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(Integer employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public Date getDay() {
+        return day;
+    }
+
+    public void setDay(Date day) {
+        this.day = day;
+    }
+
+    public String getTimeType() {
+        return timeType;
+    }
+
+    public void setTimeType(String timeType) {
+        this.timeType = timeType;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getStartType() {
+        return startType;
+    }
+
+    public void setStartType(String startType) {
+        this.startType = startType;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getEndType() {
+        return endType;
+    }
+
+    public void setEndType(String endType) {
+        this.endType = endType;
+    }
+
+    public String getWorkType() {
+        return workType;
+    }
+
+    public void setWorkType(String workType) {
+        this.workType = workType;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public String toString() {
+        return "Attendance{" +
+                "id=" + id +
+                ", employeeNumber=" + employeeNumber +
+                ", day=" + day +
+                ", timeType='" + timeType + '\'' +
+                ", startTime=" + startTime +
+                ", startType='" + startType + '\'' +
+                ", endTime=" + endTime +
+                ", endType='" + endType + '\'' +
+                ", workType='" + workType + '\'' +
+                ", notes='" + notes + '\'' +
+                ", employee=" + employee +
+                '}';
     }
 }
---
+++ Hospital/src/main/java/com/hospital_hr/entity/Employee.java
@@ -0,0 +1,8 @@
+package com.hospital_hr.entity;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午11:15 on 17/1/23.
+ */
+public class Employee {
+}
