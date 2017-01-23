9a5f85019c74404fda9cfab3d68b7dc52b90d46d
StayHungryStayFoolish <bonismo@hotmail.com>
2017-01-23 23:14:29 +0800

    commit

--- Hospital/src/main/java/com/hospital_hr/entity/Attendance.java
+++ Hospital/src/main/java/com/hospital_hr/entity/Attendance.java
@@ -10,27 +10,31 @@ import java.util.Date;
 /**
  * Created by bonismo@hotmail.com
  * 下午11:32 on 17/1/22.
+ *
+ * 考勤表
  */
 @TableName("attendance")
 public class Attendance extends Model<Attendance> implements Serializable{
 
     @TableId
     private Integer id;
+    // 员工编号
     private Integer employeeNumber;
-    private String name;
-    private String gender;
-    private Date birthday;
-    private String telephone;
-    private String email;
-    private String address;
-    private String photo;
-    private String education;
-    private Date inTime;
-    private Date outTime;
-    private Integer departmentNumber;
-    private Integer positionNumber;
-    private String status;
-    private String home;
+    // 日期
+    private Date day;
+    // 上班状态 '上午', '下午', '加班'
+    private String timeType;
+    // 上班时间
+    private Date startTime;
+    // 上班考勤状态 '正常', '迟到', '未签到'
+    private String startType;
+    // 下班时间
+    private Date endTime;
+    // 下班考勤状态 '正常', '早退', '未签到'
+    private String endType;
+    // 工作状态 上班', '请假'
+    private String workType;
+    // 日志
     private String notes;
 
     @Override
