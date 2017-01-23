d6ed95ca807ee28902449cf1eefb29f2ff3f796e
StayHungryStayFoolish <bonismo@hotmail.com>
2017-01-23 23:39:59 +0800

    commit

--- Hospital/src/main/java/com/hospital_hr/entity/Employee.java
+++ Hospital/src/main/java/com/hospital_hr/entity/Employee.java
@@ -1,6 +1,7 @@
 package com.hospital_hr.entity;
 
 import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableId;
 import com.baomidou.mybatisplus.annotations.TableName;
 
@@ -47,10 +48,173 @@ public class Employee extends Model<Employee> implements Serializable {
     // 备注
     private String notes;
 
+    @TableField(exist = false)
+    private Department department;
 
+    @TableField(exist = false)
+    private Posistion posistion;
 
     @Override
     protected Serializable pkVal() {
         return null;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(Integer employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public Integer getDepartmentNumber() {
+        return departmentNumber;
+    }
+
+    public void setDepartmentNumber(Integer departmentNumber) {
+        this.departmentNumber = departmentNumber;
+    }
+
+    public Integer getPositionNumber() {
+        return positionNumber;
+    }
+
+    public void setPositionNumber(Integer positionNumber) {
+        this.positionNumber = positionNumber;
+    }
+
+    public Date getInTime() {
+        return inTime;
+    }
+
+    public void setInTime(Date inTime) {
+        this.inTime = inTime;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Posistion getPosistion() {
+        return posistion;
+    }
+
+    public void setPosistion(Posistion posistion) {
+        this.posistion = posistion;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", employeeNumber=" + employeeNumber +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", birthday=" + birthday +
+                ", telephone='" + telephone + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", photo='" + photo + '\'' +
+                ", education='" + education + '\'' +
+                ", departmentNumber=" + departmentNumber +
+                ", positionNumber=" + positionNumber +
+                ", inTime=" + inTime +
+                ", password='" + password + '\'' +
+                ", notes='" + notes + '\'' +
+                ", department=" + department +
+                ", posistion=" + posistion +
+                '}';
     }
 }
