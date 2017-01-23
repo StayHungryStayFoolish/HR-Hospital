248f188514f60a88263f17c32d206ff9220d511d
StayHungryStayFoolish <bonismo@hotmail.com>
2017-01-23 23:27:29 +0800

    commit

--- Hospital/src/main/java/com/hospital_hr/entity/Department.java
+++ Hospital/src/main/java/com/hospital_hr/entity/Department.java
@@ -25,12 +25,82 @@ public class Department extends Model<Department> implements Serializable {
     private String manager;
     // 联系电话
     private String telephone;
-    //
+    // 部门地址
     private String address;
+    // 日志
     private String notes;
 
     @Override
     protected Serializable pkVal() {
         return null;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getDepartmentNumber() {
+        return departmentNumber;
+    }
+
+    public void setDepartmentNumber(Integer departmentNumber) {
+        this.departmentNumber = departmentNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManager() {
+        return manager;
+    }
+
+    public void setManager(String manager) {
+        this.manager = manager;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", departmentNumber=" + departmentNumber +
+                ", name='" + name + '\'' +
+                ", manager='" + manager + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", address='" + address + '\'' +
+                ", notes='" + notes + '\'' +
+                '}';
     }
 }
