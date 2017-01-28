aff4f070f5bdf3409758138f22fbb65cf6400b9d
StayHungryStayFoolish <bonismo@hotmail.com>
2017-01-28 16:48:38 +0800

    commit

--- Hospital/src/main/java/com/hospital_hr/entity/RewardsPunishment.java
+++ Hospital/src/main/java/com/hospital_hr/entity/RewardsPunishment.java
@@ -35,4 +35,68 @@ public class RewardsPunishment extends Model<RewardsPunishment> implements Seria
     protected Serializable pkVal() {
         return null;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(Integer employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public Float getMoney() {
+        return money;
+    }
+
+    public void setMoney(Float money) {
+        this.money = money;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getManager() {
+        return manager;
+    }
+
+    public void setManager(String manager) {
+        this.manager = manager;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
