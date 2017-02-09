e427b767942a89176bb66a7fe094d73f3ed0917c
StayHungryStayFoolish <bonismo@hotmail.com>
2017-02-09 22:15:56 +0800

    commit

--- Hospital/src/main/java/com/hospital_hr/uitl/MyTimeUtil.java
+++ Hospital/src/main/java/com/hospital_hr/uitl/MyTimeUtil.java
@@ -1,6 +1,6 @@
 package com.hospital_hr.uitl;
 
-import javax.annotation.Resource;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -39,6 +39,27 @@ public class MyTimeUtil {
             SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
             String string = format.format(date);
             return string;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 日期字符串转日期
+     *
+     * @param string
+     * @return
+     */
+    public static Date stringDateParse(String string) {
+        if (null != string && !"".equals(string)) {
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            Date date = null;
+            try {
+                date = format.parse(string);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return date;
         } else {
             return null;
         }
