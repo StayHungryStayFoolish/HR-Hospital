a88df6b859c73c70bb44bf92ec9cee588b99f486
StayHungryStayFoolish <bonismo@hotmail.com>
2017-02-15 23:32:42 +0800

    commit

---
+++ Hospital/src/main/java/com/hospital_hr/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,63 @@
+package com.hospital_hr.service.impl;
+
+import com.baomidou.mybatisplus.plugins.Page;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.hospital_hr.entity.Employee;
+import com.hospital_hr.mapper.EmployeeMapper;
+import com.hospital_hr.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午11:31 on 17/2/15.
+ */
+@Service
+public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> implements EmployeeService {
+
+    @Override
+    public Employee checkLogin(Integer employeeNumber, String password) {
+        return null;
+    }
+
+    @Override
+    public Page<Employee> selectListByPage(int page) {
+        return null;
+    }
+
+    @Override
+    public void addEmployee(Employee employee) {
+
+    }
+
+    @Override
+    public Employee selectEmployee(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void updateEmployee(Employee employee, String status, String manager) {
+
+    }
+
+    @Override
+    public void deleteEmployee(Integer id) {
+
+    }
+
+    @Override
+    public Employee selectByNumber(Integer employeeNumber) {
+        return null;
+    }
+
+    @Override
+    public Page<Employee> search(String input, int page) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> select(Integer employeeNumber, String password) {
+        return null;
+    }
+}
