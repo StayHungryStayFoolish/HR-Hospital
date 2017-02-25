4f46643fa9afe643d4f101b4455ed544602c7cd6
StayHungryStayFoolish <bonismo@hotmail.com>
2017-02-25 22:14:03 +0800

    commit

---
+++ Hospital/src/main/java/com/hospital_hr/mapper/OvertimeMapper.java
@@ -0,0 +1,11 @@
+package com.hospital_hr.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.hospital_hr.entity.Overtime;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午10:12 on 17/2/25.
+ */
+public interface OvertimeMapper extends BaseMapper<Overtime> {
+}
---
+++ Hospital/src/main/java/com/hospital_hr/mapper/RewardsPunishmentMapper.java
@@ -0,0 +1,11 @@
+package com.hospital_hr.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.hospital_hr.entity.RewardsPunishment;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午10:13 on 17/2/25.
+ */
+public interface RewardsPunishmentMapper extends BaseMapper<RewardsPunishment> {
+}
