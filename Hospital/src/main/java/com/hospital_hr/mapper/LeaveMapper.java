2968abb8d0aeb8bb2bff9dd3866834887e7f4787
StayHungryStayFoolish <bonismo@hotmail.com>
2017-02-25 22:11:27 +0800

    commit

---
+++ Hospital/src/main/java/com/hospital_hr/mapper/LeaveMapper.java
@@ -0,0 +1,11 @@
+package com.hospital_hr.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.hospital_hr.entity.Leave;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午10:09 on 17/2/25.
+ */
+public interface LeaveMapper extends BaseMapper<Leave> {
+}
