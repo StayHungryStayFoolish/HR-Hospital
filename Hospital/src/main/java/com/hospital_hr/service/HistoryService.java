4c8775a2083763edca64b5bdb15088a90251cb53
StayHungryStayFoolish <bonismo@hotmail.com>
2017-02-26 23:44:58 +0800

    commit

---
+++ Hospital/src/main/java/com/hospital_hr/service/HistoryService.java
@@ -0,0 +1,12 @@
+package com.hospital_hr.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.hospital_hr.entity.History;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午11:29 on 17/2/26.
+ */
+public interface HistoryService extends IService<History> {
+    
+}
