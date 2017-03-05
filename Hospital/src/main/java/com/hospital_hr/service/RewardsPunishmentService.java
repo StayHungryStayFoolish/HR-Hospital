2660f8bb58c7b2d8affdb09096321a03ffb9c220
StayHungryStayFoolish <bonismo@hotmail.com>
2017-03-05 23:34:31 +0800

    commit

---
+++ Hospital/src/main/java/com/hospital_hr/service/RewardsPunishmentService.java
@@ -0,0 +1,11 @@
+package com.hospital_hr.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.hospital_hr.entity.RewardsPunishment;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午11:45 on 17/3/5.
+ */
+public interface RewardsPunishmentService extends IService<RewardsPunishment> {
+}
---
+++ Hospital/src/main/java/com/hospital_hr/service/impl/RewardsPunishmentServiceImpl.java
@@ -0,0 +1,15 @@
+package com.hospital_hr.service.impl;
+
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.hospital_hr.entity.RewardsPunishment;
+import com.hospital_hr.mapper.RewardsPunishmentMapper;
+import com.hospital_hr.service.RewardsPunishmentService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by bonismo@hotmail.com
+ * 下午11:47 on 17/3/5.
+ */
+@Service
+public class RewardsPunishmentServiceImpl extends ServiceImpl<RewardsPunishmentMapper, RewardsPunishment> implements RewardsPunishmentService {
+}
