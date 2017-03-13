50d4c23b13808cd206af7bae4c7e8b24a45a2384
StayHungryStayFoolish <bonismo@hotmail.com>
2017-03-13 22:37:38 +0800

    commit

--- Hospital/src/main/java/com/hospital_hr/controller/AttendanceController.java
+++ Hospital/src/main/java/com/hospital_hr/controller/AttendanceController.java
@@ -1,9 +1,13 @@
 package com.hospital_hr.controller;
 
+import com.hospital_hr.entity.Attendance;
 import com.hospital_hr.service.AttendanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by bonismo@hotmail.com
@@ -16,10 +20,41 @@ public class AttendanceController {
     @Autowired
     private AttendanceService attendanceService;
 
-    @RequestMapping("index")
-    public String index() {
-        System.out.println("进入 index 方法");
-        return "redirect:/jsp/index.jsp";
+    /**
+     * 签到
+     *
+     * @param employeeNumber
+     * @return
+     */
+    @RequestMapping("/addStart.do")
+    public String addStart(Integer employeeNumber) {
+        attendanceService.addStart(employeeNumber);
+        return "welcome";
+    }
+
+    /**
+     * 签退
+     *
+     * @param employeeNumber
+     * @return
+     */
+    @RequestMapping("/addEnd.do")
+    public String addEnd(Integer employeeNumber) {
+        attendanceService.addEnd(employeeNumber);
+        return "welcome";
+    }
+
+    /**
+     * 查询所有员工考勤记录
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping("/list.do")
+    public String selectList(Model model) {
+        List<Attendance> attendanceList = attendanceService.selectList();
+        model.addAttribute("aList", attendanceList);
+        return "admin/attendance_lsit";
     }
 
 }
