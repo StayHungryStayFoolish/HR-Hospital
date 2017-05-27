9bd1e462c8c2b2f867eb0fa19fc44cfa6377c0ab
StayHungryStayFoolish <bonismo@hotmail.com>
2017-05-27 23:41:01 +0800

    commit

--- Hospital/src/main/java/com/hospital_hr/controller/EmployeeController.java
+++ Hospital/src/main/java/com/hospital_hr/controller/EmployeeController.java
@@ -12,13 +12,11 @@ import com.hospital_hr.service.HistoryService;
 import com.hospital_hr.service.PositionService;
 import com.hospital_hr.uitl.MyTimeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
